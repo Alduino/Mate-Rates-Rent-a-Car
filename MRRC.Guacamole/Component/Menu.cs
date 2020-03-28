2141b4adb9e9e19b99b7054469b0a1db2e4b1be4
Alduino <git@alduino.dev>
2020-03-28 13:21:42 +1000

    add components to render menus

---
+++ MRRC.Guacamole/Component/Menu.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+
+namespace MRRC.Guacamole.Component
+{
+    public class Menu<T> : IComponent
+    {
+        public event EventHandler<char> KeyPressed;
+        public event EventHandler<T> ItemEntered;
+
+        private int _highlightIndex;
+        
+        public string Name { get; }
+        public T[] Items { get; }
+        public int Width { get; }
+
+        public T ActiveItem => Items[_highlightIndex];
+        
+        public Menu(string name, T[] items, int width = 32)
+        {
+            Name = name;
+            Items = items;
+            Width = width;
+        }
+
+        public void Render(int x, int y, bool active = true)
+        {
+            Console.CursorVisible = false;
+
+            DrawUtil.Outline(x, y, Width, Console.WindowHeight);
+            DrawUtil.Lines(x + 2, y, 
+                Items
+                    .Select(item => item.ToString())
+                    .Select(item => item.Length > Width - 4 ? 
+                        item.Substring(0, Width - 5) + "…" : 
+                        item));
+            
+            Console.CursorVisible = true;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ MRRC.Guacamole/Component/MenuManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MRRC.Guacamole.Component
+{
+    public class MenuManager : IComponent
+    {
+        public Menu<IComponent> RootMenu { get; }
+
+        public MenuManager(Menu<IComponent> rootMenu)
+        {
+            RootMenu = rootMenu;
+        }
+
+        public void Render(int x, int y, bool active = true)
+        {
+            var menuItems = new List<IComponent>(new [] { RootMenu });
+
+            while (true)
+            {
+                var currentItem = menuItems.Last();
+
+                if (currentItem is Menu<IComponent> menuItem)
+                {
+                    var nextItem = menuItem.ActiveItem;
+                    menuItems.Add(nextItem);
+                }
+                else break;
+            }
+
+            // subtract one from menu width so that they overlap on the border line
+            var totalWidth = menuItems.Sum(v => v is Menu<IComponent> menu ? menu.Width - 1 : 16);
+            var scroll = Math.Min(0, Console.WindowWidth - totalWidth);
+
+            var offset = scroll;
+            for (var i = 0; i < menuItems.Count; i++)
+            {
+                var item = menuItems[i];
+                
+                // if the item is a menu item, use its width
+                // otherwise, if it is the last item, fill the available space
+                // otherwise default to a width of 16
+                var width = item is Menu<IComponent> menu ? menu.Width :
+                    i == menuItems.Count - 1 ? Console.WindowWidth - totalWidth + 16 : 16;
+
+                item.Render(x + offset, y, active && i == menuItems.Count - 1);
+                offset += x;
+            }
+        }
+
+        public event EventHandler<char> KeyPressed;
+    }
+}
