42a50cb9d77592ee49663172a374b46fe4dc2252
Alduino <git@alduino.dev>
2020-03-29 14:17:56 +1000

    Add documentation to `KeyPressEvent`

--- MRRC.Guacamole/KeyPressEvent.cs
+++ MRRC.Guacamole/KeyPressEvent.cs
@@ -5,8 +5,19 @@ namespace MRRC.Guacamole
 {
     public class KeyPressEvent : CancelEventArgs
     {
+        /// <summary>
+        /// The current state of the application
+        /// </summary>
         public ApplicationState State;
+        
+        /// <summary>
+        /// Information about the keys that were pressed
+        /// </summary>
         public ConsoleKeyInfo Key;
+        
+        /// <summary>
+        /// Set this to true to trigger a render once the event completes
+        /// </summary>
         public bool Rerender;
     }
 }
