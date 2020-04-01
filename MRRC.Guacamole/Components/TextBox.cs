d5a6f15221a32b0a7831bf21ab29a6d19ea67193
Alduino <git@alduino.dev>
2020-04-01 12:11:24 +1000

    Fix textbox

--- MRRC.Guacamole/Components/TextBox.cs
+++ MRRC.Guacamole/Components/TextBox.cs
@@ -5,7 +5,7 @@ namespace MRRC.Guacamole.Components
     public class TextBox : Component
     {
         public string Label { get; }
-        public string Value { get; private set; }
+        public string Value { get; private set; } = "";
 
         private int _width = 30;
         public int Width
@@ -28,10 +28,25 @@ namespace MRRC.Guacamole.Components
 
         private void UpdateValue(object sender, KeyPressEvent e)
         {
-            if (e.Key.Key == ConsoleKey.Backspace && Value.Length > 0)
+            e.Cancel = true;
+            
+            switch (e.Key.Key)
             {
-                Value = Value.Substring(0, Value.Length - 1);
-                return;
+                case ConsoleKey.LeftArrow when Value.Length == 0:
+                    // Exit out of focus to the parent component
+                    e.State.ActiveComponent = Parent;
+                    e.Rerender = true;
+                    return;
+                case ConsoleKey.Backspace:
+                {
+                    if (Value.Length <= 0) e.Rerender = true;
+                    else Value = Value.Substring(0, Value.Length - 1);
+                    if (Value.Length > Width - 4) e.Rerender = true;
+                    return;
+                }
+                case ConsoleKey.Enter:
+                    e.Rerender = true;
+                    return;
             }
 
             if (e.Key.KeyChar == 0)
@@ -40,12 +55,14 @@ namespace MRRC.Guacamole.Components
             }
             
             Value += e.Key.KeyChar;
+
+            if (Value.Length > Width - 3) e.Rerender = true;
         }
 
         protected override void Draw(int x, int y, bool active, ApplicationState state)
         {
             DrawUtil.Outline(x, y, Width, 3, Label);
-            Console.Write(Value);
+            Console.Write(Value.Substring(Math.Max(0, Value.Length - Width + 3)), Value.Length);
         }
 
         public override string ToString() => Label;
