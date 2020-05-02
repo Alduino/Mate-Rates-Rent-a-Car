639437ac486efc34fa55bc311d3cdf373c415858
Alduino <git@alduino.dev>
2020-05-02 15:35:04 +1000

    Use correct namespace for data project
    MCCR had the Rs and Cs swapped

--- MRRC.Data/Properties/AssemblyInfo.cs
+++ MRRC.Data/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCCR.Data")]
+[assembly: AssemblyTitle("MRRC.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zach Barham")]
-[assembly: AssemblyProduct("MCCR.Data")]
+[assembly: AssemblyProduct("MRRC.Data")]
 [assembly: AssemblyCopyright("Copyright © Zach Barham 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
