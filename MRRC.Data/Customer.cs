eb42be9400c1d5ce1ab3e8d33c3dc4f5c451f01a
Alduino <git@alduino.dev>
2020-05-03 11:58:14 +1000

    Use correct last name header

--- MRRC.Data/Customer.cs
+++ MRRC.Data/Customer.cs
@@ -8,7 +8,7 @@ namespace MCCR.Data
         [Name("ID")] public int Id { get; set; }
         public string Title { get; set; }
         [Name("FirstName")] public string GivenNames { get; set; }
-        [Name("Surname")] public string Surname { get; set; }
+        [Name("LastName")] public string Surname { get; set; }
         public Gender Gender { get; set; }
         [Name("DOB")] public DateTime BirthDate { get; set; }
     }
