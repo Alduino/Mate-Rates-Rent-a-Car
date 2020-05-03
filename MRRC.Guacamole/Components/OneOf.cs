bddea7d1f95d5925e660d111cca4c7d3eba08185
Alduino <git@alduino.dev>
2020-05-03 13:11:08 +1000

    Add one-of component
    Renders one of its children, as set by some other piece of code

---
+++ MRRC.Guacamole/Components/OneOf.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace MRRC.Guacamole.Components
+{
+    public class OneOf : Component
+    {
+        private readonly Dictionary<string, Component> _components;
+
+        public OneOf(Dictionary<string, Component> components, string initial)
+        {
+            _components = components;
+
+            ActiveComponent = initial;
+
+            foreach (var c in _components)
+            {
+                c.Value.SetChildOf(this);
+            }
+
+            Focused += (_, ev) =>
+            {
+                ActiveComponent = initial;
+                ev.State.ActiveComponent = CurrentComponent;
+            };
+        }
+
+        public Component CurrentComponent => _components[ActiveComponent];
+        
+        public string ActiveComponent { get; set; }
+
+        protected override void Draw(int x, int y, bool active, ApplicationState state)
+        {
+            if (!_components.ContainsKey(ActiveComponent)) 
+                throw new IndexOutOfRangeException("Invalid active component");
+            
+            var component = _components[ActiveComponent];
+            component.Render(state, x, y);
+        }
+    }
+}
