ab6de9962f1e316b0d5bae2b5ce18ad2fe846014
Alduino <git@alduino.dev>
2020-05-04 00:12:44 +1000

    Add more vehicle actios

--- MatesRatesRentACar/FleetManager.cs
+++ MatesRatesRentACar/FleetManager.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using MCCR.Data;
+using MRRC.Guacamole;
+using MRRC.Guacamole.Components;
 using MRRC.Guacamole.Components.Forms;
 using MRRC.Guacamole.MenuGeneration;
 
@@ -27,10 +32,207 @@ namespace MateRatesRentACar
             new Form.Item("Colour", new TextBox { Placeholder = "Black"})
         }, new Button("Submit"));
 
+        [MenuItem] public OneOf ModifyVehicle { get; } = new OneOf(new Dictionary<string, Component>
+        {
+            {
+                "search",
+                new Form("Find Vehicle", new []
+                {
+                    new Form.Item("Registration", new TextBox { Placeholder = "123ABC" }), 
+                }, new Button("Search"))
+            },
+            {
+                "modify",
+                new Form("Update Vehicle", new []
+                {
+                    new Form.Item("Registration", new TextBox { ReadOnly = true}), 
+                    new Form.Item("Grade", new Select<VehicleGrade>()), 
+                    new Form.Item("Make", new TextBox()), 
+                    new Form.Item("Model", new TextBox()), 
+                    new Form.Item("Year", new TextBox { MaxLength = 4}), 
+                    new Form.Item("Seat count", new TextBox { Placeholder = "Default: 4"}), 
+                    new Form.Item("Transmission", new Select<TransmissionType>().WithDefault(TransmissionType.Manual)), 
+                    new Form.Item("Fuel type", new Select<FuelType>().WithDefault(FuelType.Petrol)), 
+                    new Form.Item("Has GPS", new Checkbox()), 
+                    new Form.Item("Has sunroof", new Checkbox()), 
+                    new Form.Item("Daily rate $", new TextBox { Placeholder = "Default: 50.0"}), 
+                    new Form.Item("Colour", new TextBox { Placeholder = "Black"})
+                }, new Button("Submit"))
+            }
+        }, "search", "Modify Vehicle");
+
+        [MenuItem] public OneOf DeleteVehicle { get; } = new OneOf(new Dictionary<string, Component>
+        {
+            {
+                "select",
+                new Form("Find Vehicle", new []
+                {
+                    new Form.Item("Registration", new TextBox { Placeholder = "123ABC"}), 
+                }, new Button("Search"))
+            },
+            {
+                "confirm",
+                new Form("Confirm Deletion", new []
+                {
+                    new Form.Item("Registration", new TextBox { ReadOnly = true }), 
+                    new Form.Item("Make", new TextBox { ReadOnly = true }), 
+                    new Form.Item("Model", new TextBox { ReadOnly = true }), 
+                    new Form.Item("Year", new TextBox { ReadOnly = true }), 
+                }, new Button("Confirm"))
+            }
+        }, "select", "Delete Vehicle");
+
         public FleetManager(Fleet fleet)
         {
             _fleet = fleet;
             AddVehicle.Submitted += AddVehicleOnSubmitted;
+            ModifyVehicle.GetComponent<Form>("search").Submitted += ModifyVehicleOnSearch;
+            ModifyVehicle.GetComponent<Form>("modify").Submitted += ModifyVehicleOnSubmit;
+            DeleteVehicle.GetComponent<Form>("select").Submitted += DeleteVehicleOnSearch;
+            DeleteVehicle.GetComponent<Form>("confirm").Submitted += DeleteVehicleOnSubmitted;
+        }
+
+        private void DeleteVehicleOnSubmitted(object sender, Form.SubmittedEventArgs e)
+        {
+            if (!_fleet.RemoveVehicle(e.Data.Get<string>("Registration")))
+            {
+                e.Result = "Could not delete";
+            }
+            else
+            {
+                e.Result = "Deleted vehicle";
+            }
+        }
+
+        private void DeleteVehicleOnSearch(object sender, Form.SubmittedEventArgs e)
+        {
+            var rego = e.Data.Get<string>("Registration").ToUpper();
+            if (!RegoRegex.IsMatch(rego))
+            {
+                e.Result = "Invalid rego";
+                return;
+            }
+
+            if (_fleet.Vehicles.All(v => v.Registration != rego))
+            {
+                e.Result = "No vehicle with that rego";
+                return;
+            }
+
+            if (_fleet.IsRented(rego))
+            {
+                e.Result = "Vehicle is under rent";
+                return;
+            }
+
+            var vehicle = _fleet.Vehicles.First(v => v.Registration == rego);
+
+            var confirmForm = DeleteVehicle.GetComponent<Form>("confirm");
+            confirmForm.Set("Registration", rego);
+            confirmForm.Set("Make", vehicle.Make);
+            confirmForm.Set("Model", vehicle.Model);
+            confirmForm.Set("Year", vehicle.Year.ToString());
+
+            DeleteVehicle.ActiveComponent = "confirm";
+        }
+
+        private void ModifyVehicleOnSubmit(object sender, Form.SubmittedEventArgs e)
+        {
+            if (!e.Data.TryGet("Grade", out VehicleGrade grade))
+            {
+                e.Result = "Grade is required";
+                return;
+            }
+
+            if (!e.Data.TryGet("Make", out string make) || make.IsEmpty())
+            {
+                e.Result = "Make is required";
+                return;
+            }
+
+            if (!e.Data.TryGet("Model", out string model) || make.IsEmpty())
+            {
+                e.Result = "Model is required";
+                return;
+            }
+
+            if (!e.Data.TryGet("Year", out string yearStr) || 
+                yearStr.Length != 4 || !int.TryParse(yearStr, out var year))
+            {
+                e.Result = "Year is invalid";
+                return;
+            }
+
+            if (!e.Data.TryGet("Seat count", out string seatCountStr) || 
+                !int.TryParse(seatCountStr.IsEmpty() ? "4" : seatCountStr, out var seatCount))
+            {
+                e.Result = "Seat count is invalid";
+                return;
+            }
+
+            if (!e.Data.TryGet("Daily rate $", out string dailyRateStr) || 
+                !double.TryParse(dailyRateStr.IsEmpty() ? "50" : dailyRateStr, out var dailyRate))
+            {
+                e.Result = $"Daily rate is invalid";
+                return;
+            }
+
+            if (!e.Data.TryGet("Colour", out string colour)) {
+                e.Result = "Colour is required";
+                return;
+            }
+
+            if (colour.IsEmpty()) colour = "Black";
+
+            var vehicle = _fleet.Vehicles
+                .First(v => v.Registration == e.Data.Get<string>("Registration"));
+
+            vehicle.Grade = grade;
+            vehicle.Make = make;
+            vehicle.Model = model;
+            vehicle.Year = year;
+            vehicle.SeatCount = seatCount;
+            vehicle.Transmission = e.Data.Get<TransmissionType>("Transmission");
+            vehicle.Fuel = e.Data.Get<FuelType>("Fuel type");
+            vehicle.HasGps = e.Data.Get<bool>("Has GPS");
+            vehicle.HasSunRoof = e.Data.Get<bool>("Has sunroof");
+            vehicle.DailyRate = dailyRate;
+            vehicle.Colour = colour;
+
+            e.Result = "Successfully updated vehicle";
+        }
+
+        private void ModifyVehicleOnSearch(object sender, Form.SubmittedEventArgs e)
+        {
+            var rego = e.Data.Get<string>("Registration");
+            if (!RegoRegex.IsMatch(rego))
+            {
+                e.Result = "Invalid rego";
+                return;
+            }
+
+            var vehicle = _fleet.Vehicles.FirstOrDefault(v => v.Registration == rego.ToUpper());
+            if (vehicle == null)
+            {
+                e.Result = "No vehicle with that rego";
+                return;
+            }
+
+            var modifyForm = ModifyVehicle.GetComponent<Form>("modify");
+            modifyForm.Set("Registration", rego);
+            modifyForm.Set("Grade", vehicle.Grade);
+            modifyForm.Set("Make", vehicle.Make);
+            modifyForm.Set("Model", vehicle.Model);
+            modifyForm.Set("Year", vehicle.Year.ToString());
+            modifyForm.Set("Seat count", vehicle.SeatCount.ToString());
+            modifyForm.Set("Transmission", vehicle.Transmission);
+            modifyForm.Set("Fuel type", vehicle.Fuel);
+            modifyForm.Set("Has GPS", vehicle.HasGps);
+            modifyForm.Set("Has sunroof", vehicle.HasSunRoof);
+            modifyForm.Set("Daily rate $", vehicle.DailyRate.ToString(CultureInfo.InvariantCulture));
+            modifyForm.Set("Colour", vehicle.Colour);
+
+            ModifyVehicle.ActiveComponent = "modify";
         }
 
         private void AddVehicleOnSubmitted(object sender, Form.SubmittedEventArgs e)
