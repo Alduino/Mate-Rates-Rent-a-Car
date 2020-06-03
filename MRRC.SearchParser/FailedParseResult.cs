55481303ce6d88982b499fc863a935bb3c474b99
Alduino <git@alduino.dev>
2020-06-03 15:27:43 +1000

    Add method to cast FailedParseResult to a different generic type
    This works because FailedParseResult doesn't actually use the generic type, other than to support the IParseResult interface.

--- MRRC.SearchParser/FailedParseResult.cs
+++ MRRC.SearchParser/FailedParseResult.cs
@@ -16,6 +16,15 @@ namespace MRRC.SearchParser
         public FailedParseResult(Token.Type expected, Token.Match found, string help) :
             this(new[] {expected}, found, help) {}
 
+        /// <summary>
+        /// Returns a FailedParseResult containing the same data, but with a different generic type
+        /// </summary>
+        /// <typeparam name="TO">New generic type for the result</typeparam>
+        public FailedParseResult<TO> Cast<TO>()
+        {
+            return new FailedParseResult<TO>(ExpectedTokens, FoundToken, Help);
+        }
+
         public bool Successful => false;
         public T Result { get; }
         public string Message => $"Expected {string.Join(", ", ExpectedTokens)}, found {FoundToken.Type}. {Help}";
