18b359b8a40e82020e34942be82b12b2f69f0637
Alduino <git@alduino.dev>
2020-06-03 21:30:23 +1000

    Add method to do Then over multiple values

--- MRRC.SearchParser/IParseResult.cs
+++ MRRC.SearchParser/IParseResult.cs
@@ -15,6 +15,23 @@ namespace MRRC.SearchParser
             var res = (SuccessfulParseResult<TI>) parseResult;
             return new SuccessfulParseResult<TV>(map(res.Result));
         }
+
+        /// <summary>
+        /// Same as <see cref="Then{TI,TV}"/>, but supports multiple items
+        /// </summary>
+        public static IParseResult<TV> AndThen<TA, TB, TC, TV>(this IParseResult<TA> a, IParseResult<TB> b,
+            IParseResult<TC> c, Func<TA, TB, TC, TV> map)
+        {
+            if (a is FailedParseResult<TA> fA) return fA.Cast<TV>();
+            if (b is FailedParseResult<TA> fB) return fB.Cast<TV>();
+            if (c is FailedParseResult<TA> fC) return fC.Cast<TV>();
+
+            var rA = (SuccessfulParseResult<TA>) a;
+            var rB = (SuccessfulParseResult<TB>) b;
+            var rC = (SuccessfulParseResult<TC>) c;
+            
+            return new SuccessfulParseResult<TV>(map(rA.Result, rB.Result, rC.Result));
+        }
     }
     
     public interface IParseResult<out T>
