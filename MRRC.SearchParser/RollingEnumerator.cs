d7d5491751af1896c8606f21246b5bee988fc994
Alduino <git@alduino.dev>
2020-06-03 16:42:26 +1000

    Add indexing
    It's O(n) so shouldn't be used if possible, but it's there if it is needed

--- MRRC.SearchParser/RollingEnumerator.cs
+++ MRRC.SearchParser/RollingEnumerator.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Runtime.CompilerServices;
+
 namespace MRRC.SearchParser
 {
     /// <summary>
     /// Linked list that allows pushing to the end and shifting out from the start
     /// </summary>
+    /// <remarks>
+    /// Provides O(1) pushing and shifting, and O(n) indexing.
+    /// </remarks>
     public class RollingEnumerator<T>
     {
         private class Item
@@ -13,12 +19,20 @@ namespace MRRC.SearchParser
                 Next = next;
             }
 
-            public T Value { get; }
+            public T Value { get; internal set; }
             public Item Next { get; set; }
         }
 
         private Item _first;
         private Item _last;
+
+        private Item GetItem(int index)
+        {
+            if (index >= Length) throw new IndexOutOfRangeException();
+            var nextItem = _first;
+            for (var i = 1; i <= index; i++) nextItem = nextItem.Next;
+            return nextItem;
+        }
 
         /// <summary>
         /// The current length of the list
@@ -61,8 +75,33 @@ namespace MRRC.SearchParser
         }
 
         /// <summary>
+        /// Returns the item at the specified index
+        /// </summary>
+        /// <remarks>
+        /// This is an O(n) operation, unlike array-based lists. If you are using this method more than pushing and
+        /// shifting, you probably want to use a normal list instead.
+        /// </remarks>
+        public T Get(int index) => GetItem(index).Value;
+
+        /// <summary>
+        /// Sets the value of the item at the specified index
+        /// </summary>
+        /// <seealso cref="Get">See Get remarks for speed</seealso>
+        public void Set(int index, T value) => GetItem(index).Value = value;
+
+        /// <summary>
         /// Returns true when there is at least one item in the list
         /// </summary>
         public bool HasItems() => _first != null;
+
+        /// <summary>
+        /// Provides O(n) indexing
+        /// </summary>
+        [IndexerName("Indexer")]
+        public T this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
     }
 }
