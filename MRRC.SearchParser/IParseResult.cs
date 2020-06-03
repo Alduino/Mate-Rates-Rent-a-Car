b7916e28e7b110c952d614de076afe3778918047
Alduino <git@alduino.dev>
2020-06-03 15:28:06 +1000

    Add extension to IParseResults to map them to a new value

--- MRRC.SearchParser/IParseResult.cs
+++ MRRC.SearchParser/IParseResult.cs
@@ -1,5 +1,22 @@
+using System;
+
 namespace MRRC.SearchParser
 {
+    public static class ParseResultExtensions
+    {
+        /// <summary>
+        /// When the parse result is failed, returns that, otherwise returns a successful parse result
+        /// containing the mapped data
+        /// </summary>
+        public static IParseResult<TV> Then<TI, TV>(this IParseResult<TI> parseResult, Func<TI, TV> map)
+        {
+            if (parseResult is FailedParseResult<TI> failure) return failure.Cast<TV>();
+            
+            var res = (SuccessfulParseResult<TI>) parseResult;
+            return new SuccessfulParseResult<TV>(map(res.Result));
+        }
+    }
+    
     public interface IParseResult<out T>
     {
         bool Successful { get; }
