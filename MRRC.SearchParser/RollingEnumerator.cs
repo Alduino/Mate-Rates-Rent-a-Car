252debed2639e785610bb26cfac1a26f6f420f25
Alduino <git@alduino.dev>
2020-06-03 16:29:53 +1000

    Add inline docs to RollingEnumerator

--- MRRC.SearchParser/RollingEnumerator.cs
+++ MRRC.SearchParser/RollingEnumerator.cs
@@ -1,5 +1,8 @@
 namespace MRRC.SearchParser
 {
+    /// <summary>
+    /// Linked list that allows pushing to the end and shifting out from the start
+    /// </summary>
     public class RollingEnumerator<T>
     {
         private class Item
@@ -17,8 +20,17 @@ namespace MRRC.SearchParser
         private Item _first;
         private Item _last;
 
+        /// <summary>
+        /// The current length of the list
+        /// </summary>
         public int Length { get; private set; }
 
+        /// <summary>
+        /// Removes the first item, and sets result to it (or null if false is returned)
+        /// </summary>
+        /// <returns>
+        /// True if there was an item left to remove, false if not
+        /// </returns>
         public bool Shift(out T result)
         {
             if (_first == null)
@@ -35,6 +47,10 @@ namespace MRRC.SearchParser
             return true;
         }
 
+        /// <summary>
+        /// Appends an item to the end of the list
+        /// </summary>
+        /// <param name="value"></param>
         public void Push(T value)
         {
             var item = new Item(value, null);
@@ -44,6 +60,9 @@ namespace MRRC.SearchParser
             Length++;
         }
 
+        /// <summary>
+        /// Returns true when there is at least one item in the list
+        /// </summary>
         public bool HasItems() => _first != null;
     }
 }
