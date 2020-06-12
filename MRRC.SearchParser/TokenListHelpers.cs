f91c50ce400691f506727cceb9658576695f94cb
Alduino <git@alduino.dev>
2020-06-12 16:19:13 +1000

    Add overload with default parameter to .Next()

--- MRRC.SearchParser/TokenListHelpers.cs
+++ MRRC.SearchParser/TokenListHelpers.cs
@@ -11,6 +11,9 @@ namespace MRRC.SearchParser
             return list.Current;
         }
 
+        public static Token.Match Next(this IEnumerator<Token.Match> list, Token.Match def) =>
+            !list.MoveNext() ? def : list.Current;
+
         // TODO lookahead support
         // This might not be needed but here's the link for my future reference:
         // https://www.codeproject.com/Tips/5254600/LookAheadEnumerator-Implement-Backtracking-in-Your
