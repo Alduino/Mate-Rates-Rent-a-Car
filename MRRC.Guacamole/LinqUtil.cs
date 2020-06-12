a6bdf3b9e409783a2075d1f6e19b9b0ebfb0e1d4
Alduino <git@alduino.dev>
2020-06-12 20:13:13 +1000

    Add utility to add a range of items to a collection

--- MRRC.Guacamole/LinqUtil.cs
+++ MRRC.Guacamole/LinqUtil.cs
@@ -14,5 +14,13 @@ namespace MRRC.Guacamole
         /// Appends an item to the end of the enumerator
         /// </summary>
         public static IEnumerable<T> Append<T>(this IEnumerable<T> source, T next) => source.Concat(new[] {next});
+
+        /// <summary>
+        /// Adds the specified items to the end of the collection
+        /// </summary>
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items) collection.Add(item);
+        }
     }
 }
